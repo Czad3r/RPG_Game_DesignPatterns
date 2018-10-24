74c7e954f41ee5af4c2cc1ae88c8a0172f165750
czader98 <czader98@poczta.fm>
2018-10-24 20:34:53 +0200

    Added 2 skills with Builders

---
+++ src/gameModel/MagicAttackSkill.java
@@ -0,0 +1,35 @@
+package gameModel;
+
+public class MagicAttackSkill extends Skill {
+    int attackMultiplier;
+    int chanceToStun;
+
+    private MagicAttackSkill(){}
+    private MagicAttackSkill(Builder builder){
+        if(builder==null)return;
+        name=builder.name;
+        attackMultiplier=builder.attackMultiplier;
+        chanceToStun=builder.chanceToStun;
+    }
+
+    public static class Builder{
+        private String name;
+        private int attackMultiplier;
+        private int chanceToStun;
+
+        public Builder(String name){
+            this.name=name;
+        }
+
+        public Builder chanceToStun(int chance){
+            if(chance>0)chanceToStun=chance;
+            return this;
+        }
+        public Builder attackMultiplier(int attack){
+            if(attack>0)attackMultiplier=attack;
+            return this;
+        }
+    }
+
+
+}
